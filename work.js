0173ee2ca78d97727b3e4a0647bad0c869b6bb74
02-ahmed <ahmedboabae2@gmail.com>
2024-03-20 23:42:15 +0000

    some revisions

---
+++ work.js
@@ -0,0 +1,20 @@
+const num = 2;
+
+function test(num) {
+  let a=2;
+  let b=2;
+  let c = 2;
+  let d = 5
+  if (a !==num) {
+    return false
+  };
+  if (b !==num) {
+    return false
+  };
+  if (c !==num) {
+    return false
+  };
+  return true
+};
+
+console.log(test(num));
